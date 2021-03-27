1544f4a9b5c7d4d980729cd82a013fcafdd6af1a
HuyNguyenAu <huynguyenau@gmail.com>
2021-03-27 16:20:53 +1100

    Moved class out.

---
+++ lib/jar.dart
@@ -0,0 +1,11 @@
+class Jar {
+  String name = '';
+  double balance = 0;
+  double goal = 0;
+
+  Jar(String name, double balance, double goal) {
+    this.name = name;
+    this.balance = balance;
+    this.goal = goal;
+  }
+}
